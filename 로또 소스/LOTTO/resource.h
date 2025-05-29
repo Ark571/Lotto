4302a72dc15c792a82d181762882010d69ebabaa
Ark571 <pok9491@naver.com>
2025-05-29 12:19:34 +0900

    Add files via upload

---
+++ 로또 소스/LOTTO/resource.h
@@ -0,0 +1,79 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// LOTTO.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_LOTTO_DIALOG                102
+#define IDR_MAINFRAME                   128
+#define IDC_CHECK1                      1000
+#define IDC_CHECK2                      1001
+#define IDC_CHECK3                      1002
+#define IDC_CHECK4                      1003
+#define IDC_CHECK5                      1004
+#define IDC_CHECK6                      1005
+#define IDC_CHECK7                      1006
+#define IDC_CHECK8                      1007
+#define IDC_CHECK9                      1008
+#define IDC_CHECK10                     1009
+#define IDC_CHECK11                     1010
+#define IDC_CHECK12                     1011
+#define IDC_CHECK13                     1012
+#define IDC_CHECK14                     1013
+#define IDC_CHECK15                     1014
+#define IDC_CHECK16                     1015
+#define IDC_CHECK17                     1016
+#define IDC_CHECK18                     1017
+#define IDC_CHECK19                     1018
+#define IDC_CHECK20                     1019
+#define IDC_CHECK21                     1020
+#define IDC_CHECK22                     1021
+#define IDC_CHECK23                     1022
+#define IDC_CHECK24                     1023
+#define IDC_CHECK25                     1024
+#define IDC_CHECK26                     1025
+#define IDC_CHECK27                     1026
+#define IDC_CHECK28                     1027
+#define IDC_CHECK29                     1028
+#define IDC_CHECK30                     1029
+#define IDC_CHECK31                     1030
+#define IDC_CHECK32                     1031
+#define IDC_CHECK33                     1032
+#define IDC_CHECK34                     1033
+#define IDC_CHECK35                     1034
+#define IDC_CHECK36                     1035
+#define IDC_CHECK37                     1036
+#define IDC_CHECK38                     1037
+#define IDC_CHECK39                     1038
+#define IDC_CHECK40                     1039
+#define IDC_CHECK41                     1040
+#define IDC_CHECK42                     1041
+#define IDC_CHECK43                     1042
+#define IDC_CHECK44                     1043
+#define IDC_CHECK45                     1044
+#define IDC_BUTT_PRINT_LOTTO            1045
+#define IDC_BUTT_ZERO_LOTTO             1047
+#define IDC_BUTT_EXIT_LOTTO             1048
+#define IDC_LIST1                       1049
+#define IDC_LIST2                       1050
+#define IDC_LIST3                       1051
+#define IDC_LIST4                       1052
+#define IDC_LIST5                       1053
+#define IDC_LIST6                       1054
+#define IDC_LIST7                       1055
+#define IDC_LIST8                       1056
+#define IDC_BUTT_OFF                    1057
+#define IDC_BUTTON2                     1058
+#define IDC_BUTT_ON                     1058
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1059
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
